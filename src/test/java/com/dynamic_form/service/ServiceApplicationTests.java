1fffbbfb96b27a6d32117e92cdae9b6abae6f49a
unknown <bottupallysainath@gmail.com>
2025-08-09 11:53:57 +0530

    Initial project setup with spring initializr

---
+++ src/test/java/com/dynamic_form/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynamic_form.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
